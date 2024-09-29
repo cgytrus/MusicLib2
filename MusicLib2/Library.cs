31326b9ce10c34567563ba967616ec153f1a8748
ConfiG <cgytrus@cgyt.ru>
2024-09-29 19:39:15 +0300

    sort all tracks

--- MusicLib2/Library.cs
+++ MusicLib2/Library.cs
@@ -7,7 +7,17 @@ public readonly record struct Library(
     public static async Task<Library> Get(bool authorized) {
         Tracks tracks = Tracks.All();
         return new Library {
-            tracks = tracks.all.Values.Where(x => authorized || !string.IsNullOrEmpty(x.artist)),
+            tracks = tracks.all
+                .Where(x => authorized || !string.IsNullOrEmpty(x.Value.artist))
+                .OrderBy(x => {
+                    try {
+                        return File.GetLastWriteTimeUtc(Path.Join(Paths.music, x.Key));
+                    }
+                    catch {
+                        return DateTime.UnixEpoch;
+                    }
+                })
+                .Select(x => x.Value),
             playlists = await Playlist.All(tracks, authorized)
         };
     }
